d15597aae30b3a0ad4a3a3415556f15bf712a3e4
sachetwasti1997 <sachetwasti61@gmail.com>
2022-01-14 15:43:23 +0530

    [ADD]Top Header Completed

--- app/src/main/java/com/sachet/jettippapp/MainActivity.kt
+++ app/src/main/java/com/sachet/jettippapp/MainActivity.kt
@@ -3,11 +3,22 @@ package com.sachet.jettippapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sachet.jettippapp.ui.theme.JetTippAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +42,35 @@ fun MyApp(content: @Composable () -> Unit){
     }
 }
 
-@Preview(showBackground = true)
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 134.0){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
+
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetTippAppTheme {
